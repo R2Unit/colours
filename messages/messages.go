e39015ac6e69050c6f13ed634ae6f889e1139a62
Lorenzo Karel <lorenzok@quanza.net>
2025-01-20 23:15:07 +0100

    Small changes

--- messages/messages.go
+++ messages/messages.go
@@ -6,29 +6,27 @@ import (
 	"github.com/r2unit/colours/colours"
 )
 
-// Colourize wraps a message with the given color code.
 func Colourize(color string, message string) string {
 	return fmt.Sprintf("%s%s%s", color, message, colours.Reset)
 }
 
-// Predefined color functions with bold log level prefixes.
 var (
 	Success = func(message string) string {
-		return fmt.Sprintf("%s[Success]%s %s", Colourize(colours.Bold+colours.Green, "Success"), colours.Reset, message)
+		return fmt.Sprintf("%s[Success]%s %s%s", colours.Bold+colours.Green, colours.Reset, message, colours.Reset)
 	}
 	Warning = func(message string) string {
-		return fmt.Sprintf("%s[Warning]%s %s", Colourize(colours.Bold+colours.Yellow, "Warning"), colours.Reset, message)
+		return fmt.Sprintf("%s[Warning]%s %s%s", colours.Bold+colours.Yellow, colours.Reset, message, colours.Reset)
 	}
 	Danger = func(message string) string {
-		return fmt.Sprintf("%s[Danger]%s %s", Colourize(colours.Bold+colours.Red, "Danger"), colours.Reset, message)
+		return fmt.Sprintf("%s[Danger]%s %s%s", colours.Bold+colours.Red, colours.Reset, message, colours.Reset)
 	}
 	Info = func(message string) string {
-		return fmt.Sprintf("%s[Info]%s %s", Colourize(colours.Bold+colours.Blue, "Info"), colours.Reset, message)
+		return fmt.Sprintf("%s[Info]%s %s%s", colours.Bold+colours.Blue, colours.Reset, message, colours.Reset)
 	}
 	Debug = func(message string) string {
-		return fmt.Sprintf("%s[Debug]%s %s", Colourize(colours.Bold+colours.Cyan, "Debug"), colours.Reset, message)
+		return fmt.Sprintf("%s[Debug]%s %s%s", colours.Bold+colours.Cyan, colours.Reset, message, colours.Reset)
 	}
 	Notice = func(message string) string {
-		return fmt.Sprintf("%s[Notice]%s %s", Colourize(colours.Bold+colours.Magenta, "Notice"), colours.Reset, message)
+		return fmt.Sprintf("%s[Notice]%s %s%s", colours.Bold+colours.Magenta, colours.Reset, message, colours.Reset)
 	}
 )
