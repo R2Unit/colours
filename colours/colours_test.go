cea7629b5bc4d3f2ca3eafe86650865a408aa09e
Lorenzo Karel <lorenzok@quanza.net>
2025-01-21 06:42:11 +0100

    Working on colours.

--- colours/colours_test.go
+++ colours/colours_test.go
@@ -1,27 +1,20 @@
 package colours
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestColoursConstants(t *testing.T) {
-	tests := []struct {
-		name  string
-		value string
-	}{
-		{"Green", Green},
-		{"Yellow", Yellow},
-		{"Red", Red},
-		{"Blue", Blue},
-		{"Cyan", Cyan},
-		{"Magenta", Magenta},
-		{"Bold", Bold},
-		{"Reset", Reset},
+	if Green == "" {
+		t.Errorf("Expected Green to have a value, got empty string")
 	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if tt.value == "" {
-				t.Errorf("Expected %s to have a value, but got an empty string", tt.name)
-			}
-		})
+	if Yellow == "" {
+		t.Errorf("Expected Yellow to have a value, got empty string")
+	}
+	if Bold == "" {
+		t.Errorf("Expected Bold to have a value, got empty string")
+	}
+	if Reset == "" {
+		t.Errorf("Expected Reset to have a value, got empty string")
 	}
 }
--- messages/messages.go
+++ messages/messages.go
@@ -2,14 +2,9 @@ package messages
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/r2unit/eyes/colours"
+	"github.com/r2unit/colours/colours"
 )
-
-func getTimestamp() string {
-	return time.Now().Format("2006/01/02 15:04:05")
-}
 
 func Colourize(color string, message string) string {
 	return fmt.Sprintf("%s%s%s", color, message, colours.Reset)
@@ -17,21 +12,21 @@ func Colourize(color string, message string) string {
 
 var (
 	Success = func(message string) string {
-		return fmt.Sprintf("%s [%s]%s %s%s", getTimestamp(), Colourize(colours.Bold+colours.Green, "Success"), colours.Reset, message, colours.Reset)
+		return fmt.Sprintf("%s[Success]%s %s%s", colours.Bold+colours.Green, colours.Reset, message, colours.Reset)
 	}
 	Warning = func(message string) string {
-		return fmt.Sprintf("%s [%s]%s %s%s", getTimestamp(), Colourize(colours.Bold+colours.Yellow, "Warning"), colours.Reset, message, colours.Reset)
+		return fmt.Sprintf("%s[Warning]%s %s%s", colours.Bold+colours.Yellow, colours.Reset, message, colours.Reset)
 	}
 	Danger = func(message string) string {
-		return fmt.Sprintf("%s [%s]%s %s%s", getTimestamp(), Colourize(colours.Bold+colours.Red, "Danger"), colours.Reset, message, colours.Reset)
+		return fmt.Sprintf("%s[Danger]%s %s%s", colours.Bold+colours.Red, colours.Reset, message, colours.Reset)
 	}
 	Info = func(message string) string {
-		return fmt.Sprintf("%s [%s]%s %s%s", getTimestamp(), Colourize(colours.Bold+colours.Blue, "Info"), colours.Reset, message, colours.Reset)
+		return fmt.Sprintf("%s[Info]%s %s%s", colours.Bold+colours.Blue, colours.Reset, message, colours.Reset)
 	}
 	Debug = func(message string) string {
-		return fmt.Sprintf("%s [%s]%s %s%s", getTimestamp(), Colourize(colours.Bold+colours.Cyan, "Debug"), colours.Reset, message, colours.Reset)
+		return fmt.Sprintf("%s[Debug]%s %s%s", colours.Bold+colours.Cyan, colours.Reset, message, colours.Reset)
 	}
 	Notice = func(message string) string {
-		return fmt.Sprintf("%s [%s]%s %s%s", getTimestamp(), Colourize(colours.Bold+colours.Magenta, "Notice"), colours.Reset, message, colours.Reset)
+		return fmt.Sprintf("%s[Notice]%s %s%s", colours.Bold+colours.Magenta, colours.Reset, message, colours.Reset)
 	}
 )
